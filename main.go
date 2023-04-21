12f8295c156a1a9e5ddfc6a854864a8cb89e9fa2
Louis Royer <infos.louis.royer@gmail.com>
2023-04-21 10:15:06 +0200

    Fix go build

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 // SPDX-License-Identifier: MIT
-package srv6
+package main
 
 import (
 	"fmt"
