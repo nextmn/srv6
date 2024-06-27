0e6460e2684bbc5c5ce7a30396ee13a1dc9d44cf
Louis Royer <infos.louis.royer@gmail.com>
2024-06-27 18:55:40 +0200

    fix pk for real

--- internal/database/database.go
+++ internal/database/database.go
@@ -62,8 +62,8 @@ func NewDatabase(db *sql.DB) (*Database, error) {
 		uuid UUID PRIMARY KEY,
 		type_uplink BOOL NOT NULL,
 		enabled BOOL NOT NULL,
-		match_id INTEGER REFERENCE match(id),
-		action_id INTEGER REFERENCE action(id)
+		match_id INTEGER REFERENCES match(id),
+		action_id INTEGER REFERENCES action(id)
 		);
 	`)
 	if err != nil {
